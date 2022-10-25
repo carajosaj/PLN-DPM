0b55816d0733f7bcd00604740b1b7c669f1d97ac
carajosaj <13600374676@163.com>
2022-10-25 11:04:06 +0800

    update code

--- utils/utils.py
+++ utils/utils.py
@@ -3,7 +3,7 @@ import sys
 import json
 import pickle
 import random
-
+import torch.nn as nn
 import torch
 from tqdm import tqdm
 import numpy as np
@@ -161,38 +161,6 @@ def train_one_epoch(model, optimizer, data_loader, device, epoch):
 
     return mean_loss.item()
 
-@torch.no_grad()
-def fewevaluate(model,data_loader, device):
-    model.eval()
-
-    # 用于存储预测正确的样本个数
-    sum_num = torch.zeros(1).to(device)
-
-    data_loader = tqdm(data_loader,colour='green')
-    cout1 = torch.zeros(914)
-    allcout1 = np.array(cout1).astype(int)
-    for step, data in enumerate(data_loader):
-        images, labels = data
-        output = model(images.to(device))
-        pred = torch.max(output, dim=1)[1]
-        preds2 = pred.cpu().numpy()
-        label2 = labels.cpu().numpy()
-        total_preds1 = np.array(preds2).astype(int)
-        total_labels1 = np.array(label2).astype(int)
-        sum_num += torch.eq(pred, labels.to(device)).sum()
-        for p1, k1 in zip(total_preds1, total_labels1):
-            if (p1 == k1):
-                allcout1[p1] += 1
-
-    #     few_acc = 0
-    #     few_num = 0
-    #
-    # for p1, k1 in zip(train_label, allcout1):
-    #     if (p1 < 6):
-    #         few_acc += k1
-    #         few_num += p1
-    return sum_num.item(),allcout1
-
 
 @torch.no_grad()
 def dualevaluate(model1,model2,data_loader, device):
@@ -221,42 +189,8 @@ def dualevaluate(model1,model2,data_loader, device):
         sum_num2 += torch.eq(pred2, labels.to(device)).sum()
         sum_num_com += torch.eq(pred_com, labels.to(device)).sum()
         MAE+=mean_squared_error(labels.cpu(), pred1.cpu())
-        #F1score
-    #     pre_mask = torch.zeros(output1.size()).scatter_(1, pred1.cpu().view(-1, 1), 1.)
-    #     predict_num += pre_mask.sum(0)
-    #     tar_mask = torch.zeros(output1.size()).scatter_(1, labels.cpu().view(-1, 1), 1.)
-    #     target_num += tar_mask.sum(0)
-    #     # acc_mask = pre_mask * tar_mask
-    #     recall = acc_num / target_num
-    #     precision = acc_num / predict_num
-    # F1 = 2 * recall * precision / (recall + precision)
-    # recall = (recall.numpy()[0] * 100).round(3)
-    # precision = (precision.numpy()[0] * 100).round(3)
-    # F1 = (F1.numpy()[0] * 100).round(3)
-    # print('recall', " ".join('%s' % id for id in recall))
-    # print('precision', " ".join('%s' % id for id in precision))
-    # print('F1', " ".join('%s' % id for id in F1))
 
     return sum_num1.item(),sum_num2.item(),sum_num_com.item(),MAE/(len(data_loader)*32)
-
-@torch.no_grad()
-def evaluate(model,data_loader, device):
-    model.eval()
-
-    # 用于存储预测正确的样本个数
-    sum_num = torch.zeros(1).to(device)
-
-    data_loader = tqdm(data_loader,colour='green')
-
-    for step, data in enumerate(data_loader):
-        images, labels = data
-        output = model(images.to(device))
-        pred = torch.max(output, dim=1)[1]
-        sum_num += torch.eq(pred, labels.to(device)).sum()
-    return sum_num.item()
-
-
-
 
 def config():
     parser = argparse.ArgumentParser()
@@ -330,3 +264,132 @@ def train(train_loader, epoch, model1, optimizer1, model2, optimizer2,device,rat
     # train_acc1 = float(train_correct) / float(train_total)
     # train_acc2 = float(train_correct2) / float(train_total2)
     return train_correct, train_correct2
+
+class PenaltyLoss(nn.Module):
+    def __init__(self, cls_num_list=None, base_diversity_temperature=1.0, max_m=0.5, s=30, reweight=True,
+                 reweight_epoch=-1,
+                 base_loss_factor=1.0, additional_diversity_factor=-0.2, reweight_factor=0.05):
+        super().__init__()
+        self.base_loss = F.cross_entropy
+        self.base_loss_factor = base_loss_factor  # 1.0
+        if not reweight:
+            self.reweight_epoch = -1
+        else:
+            self.reweight_epoch = reweight_epoch  # 80
+
+        # LDAM is a variant of cross entropy and we handle it with self.m_list.
+        if cls_num_list is None:
+            # No cls_num_list is provided, then we cannot adjust cross entropy with LDAM.
+
+            self.m_list = None
+            self.per_cls_weights_enabled = None
+            self.per_cls_weights_enabled_diversity = None
+        else:
+            # We will use LDAM loss if we provide cls_num_list.
+
+            m_list = 1.0 / np.sqrt(np.sqrt(cls_num_list))
+            m_list = m_list * (max_m / np.max(m_list))
+            m_list = torch.tensor(m_list, dtype=torch.float, requires_grad=False)
+            self.m_list = m_list
+            self.s = s
+            assert s > 0
+
+            if reweight_epoch != -1:
+                idx = 1  # condition could be put in order to set idx
+                betas = [0, 0.9999]
+                effective_num = 1.0 - np.power(betas[idx], cls_num_list)
+                per_cls_weights = (1.0 - betas[idx]) / np.array(effective_num)
+                per_cls_weights = per_cls_weights / np.sum(per_cls_weights) * len(cls_num_list)
+                self.per_cls_weights_enabled = torch.tensor(per_cls_weights, dtype=torch.float, requires_grad=False)
+            else:
+                self.per_cls_weights_enabled = None
+
+            cls_num_list = np.array(cls_num_list) / np.sum(cls_num_list)
+            C = len(cls_num_list)
+            per_cls_weights = C * cls_num_list * reweight_factor + 1 - reweight_factor
+
+            # Experimental normalization: This is for easier hyperparam tuning, the effect can be described in the learning rate so the math formulation keeps the same.
+            # At the same time, the 1 - max trick that was previously used is not required since weights are already adjusted.
+            per_cls_weights = per_cls_weights / np.max(per_cls_weights)
+
+            assert np.all(per_cls_weights > 0), "reweight factor is too large: out of bounds"
+            # save diversity per_cls_weights
+            self.per_cls_weights_enabled_diversity = torch.tensor(per_cls_weights, dtype=torch.float,
+                                                                  requires_grad=False).cuda()
+
+        self.base_diversity_temperature = base_diversity_temperature
+        self.additional_diversity_factor = additional_diversity_factor
+
+    def to(self, device):
+        super().to(device)
+        if self.m_list is not None:
+            self.m_list = self.m_list.to(device)
+
+        if self.per_cls_weights_enabled is not None:
+            self.per_cls_weights_enabled = self.per_cls_weights_enabled.to(device)
+
+        if self.per_cls_weights_enabled_diversity is not None:
+            self.per_cls_weights_enabled_diversity = self.per_cls_weights_enabled_diversity.to(device)
+
+        return self
+
+    def _hook_before_epoch(self, epoch):
+        if self.reweight_epoch != -1:
+            self.epoch = epoch
+
+            if epoch > self.reweight_epoch:
+                self.per_cls_weights_base = self.per_cls_weights_enabled
+                self.per_cls_weights_diversity = self.per_cls_weights_enabled_diversity
+            else:
+                self.per_cls_weights_base = None
+                self.per_cls_weights_diversity = None
+
+    def get_final_output(self, output_logits, target):
+        x = output_logits
+
+        index = torch.zeros_like(x, dtype=torch.uint8, device=x.device)
+        index.scatter_(1, target.data.view(-1, 1), 1)
+
+        index_float = index.float()
+        batch_m = torch.matmul(self.m_list[None, :], index_float.transpose(0, 1))
+
+        batch_m = batch_m.view((-1, 1))
+        x_m = x - batch_m * self.s
+
+        final_output = torch.where(index, x_m, x)
+        return final_output
+
+    def forward(self, output_logits, target, extra_info=None):
+        if extra_info is None:
+            return self.base_loss(output_logits, target)
+
+        loss = 0
+
+        # Adding RIDE Individual Loss for each expert
+        for logits_item in extra_info['logits']:
+            ride_loss_logits = output_logits if self.additional_diversity_factor == 0 else logits_item
+            if self.m_list is None:
+                loss += self.base_loss_factor * self.base_loss(ride_loss_logits, target)
+            else:
+                final_output = self.get_final_output(ride_loss_logits, target)
+                loss += self.base_loss_factor * self.base_loss(final_output, target, weight=self.per_cls_weights_base)
+
+            base_diversity_temperature = self.base_diversity_temperature
+
+            if self.per_cls_weights_diversity is not None:
+                diversity_temperature = base_diversity_temperature * self.per_cls_weights_diversity.view((1, -1))
+                temperature_mean = diversity_temperature.mean().item()
+            else:
+                diversity_temperature = base_diversity_temperature
+                temperature_mean = base_diversity_temperature
+
+            output_dist = F.log_softmax(logits_item / diversity_temperature, dim=1)
+            with torch.no_grad():
+                # Using the mean takes only linear instead of quadratic time in computing and has only a slight difference so using the mean is preferred here
+                mean_output_dist = F.softmax(output_logits / diversity_temperature, dim=1)
+
+            loss += self.additional_diversity_factor * temperature_mean * temperature_mean * F.kl_div(output_dist,
+                                                                                                      mean_output_dist,
+                                                                                                      reduction='batchmean')
+
+        return loss
